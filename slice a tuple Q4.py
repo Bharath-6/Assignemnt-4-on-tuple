1d8c25d73eeeeae260f94a1b7241b77cf623a281
Bharath-6 <67066997+Bharath-6@users.noreply.github.com>
2020-07-02 22:06:48 +0530

    Add files via upload

---
+++ slice a tuple Q4.py
@@ -0,0 +1,5 @@
+my_tuple = ('a', 'b', 'c', 'd')
+print(my_tuple[1:]) #Print elements from index 1 to end
+print(my_tuple[:2]) #Print elements from start to index 2
+print(my_tuple[1:3]) #Print elements from index 1 to index 3
+print(my_tuple[::2]) #Print elements from start to end using step sizes of 2
---
+++ tuple convert into a stringQ3 .py
@@ -0,0 +1,18 @@
+
+# Python3 code to convert tuple  
+# into string 
+
+def convertTuple(tup): 
+
+    str =  ''.join(tup) 
+
+    return str
+
+  
+# Driver code 
+
+tuple = ('B', 'H', 'A', 'R', 'A','T','H')
+
+str = convertTuple(tuple) 
+
+print(str) 
